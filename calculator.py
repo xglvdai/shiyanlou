be6ec4f611e7b7a7b3a2331f83fc7f1405a4f0e7
xglvdai <61822985+xglvdai@users.noreply.github.com>
2020-03-15 10:41:37 +0800

    个税计算器实验楼版本
    个税计算器实验楼版本

--- calculator.py
+++ calculator.py
@@ -1,30 +1,35 @@
-#! /usr/bin/env python3
+# _*_ coding : UTF-8 _*_
+#开发人员 ： 纠结中的忐忑
+#开发时间 ： 2020/3/15  10:20
+#文件名称 ： calculator.py.PY
+#开发工具 ： PyCharm
 
 import sys
-try:
-    nub1 = int(sys.argv[1])
-    nub2 = nub1 - 5000
-    if nub2 <= 3000:
-        nub3 = nub2 * 0.03 - 0
-        print('{:.2f}'.format(nub3))
-    elif nub2 > 3000 and nub2 <= 12000:
-        nub3 = nub2 * 0.1 - 210
-        print('{:.2f}'.format(nub3))
-    elif nub2 > 12000 and nub2 <= 25000:
-        nub3 = nub2 * 0.2 - 1410
-        print('{:.2f}'.format(nub3))
-    elif nub2 > 25000 and nub2 <= 35000:
-        nub3 = nub2 * 0.25 - 2660
-        print('{:.2f}'.format(nub3))
-    elif nub2 > 35000 and nub2 <= 55000:
-        nub3 = nub2 * 0.3 - 4410
-        print('{:.2f}'.format(nub3))
-    elif nub2 > 55000 and nub2 <= 80000:
-        nub3 = nub2 * 0.35 - 7160
-        print('{:.2f}'.format(nub3))
-    elif nub2 > 80000:
-        nub3 = nub2 * 0.45 - 15160
-        print('{:.2f}'.format(nub3))
-except ValueError:
-    print("Parameter Error")
-# print('{:.2f}'.format(nub2))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Parameter Error')
+        sys.exit()
+    try:
+        income = int(sys.argv[1])
+    except ValueError:
+        print('Parameter Error')
+        sys.exit()
+    value = income - 5000
+    if value <= 0:
+        result = 0
+    elif value <= 3000:
+        result = value * 0.03 - 0
+    elif value <= 12000:
+        result = value * 0.1 -210
+    elif value <= 25000:
+        result = value * 0.2 -1410
+    elif value <= 35000:
+        result = value * 0.25 -2660
+    elif value <= 5500:
+        result = value * 0.3 - 4410
+    elif value <= 80000:
+        result = value * 0.35 - 7160
+    else:
+        result = value * 0.45 -15160
+    print(format(result,'.2f'))
